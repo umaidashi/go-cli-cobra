f813a9f6bdc916eeeb7190c8088800b32b8fe46d
umaidashi <umaidashi18@gmail.com>
2024-08-21 22:28:57 +0900

    feat: add repositories

---
+++ app/domain/repository/task_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import "github.com/umaidashi/go-cli-cobra/app/domain/model"
+
+type TaskRepository interface {
+	One(id int) (model.Task, error)
+	List() ([]model.Task, error)
+	Search(condition model.TaskSearchCondition) ([]model.Task, error)
+	Create(task model.Task) (model.Task, error)
+	Update(task model.Task) (model.Task, error)
+	Delete(task model.Task) (model.Task, error)
+}
